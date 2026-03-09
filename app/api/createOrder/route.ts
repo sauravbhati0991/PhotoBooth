3ea797ae20edadfbbf26f66d3856aa9f279138dc
Saurav Bhati <sauravbhati0991@gmail.com>
2026-03-09 14:11:16 +0530

    razorpay

--- app/api/createOrder/route.ts
+++ app/api/createOrder/route.ts
@@ -3,14 +3,15 @@ import Razorpay from "razorpay";
 
 const razorpay = new Razorpay({
   key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID as string,
-  key_secret: process.env.RAZORPAY_SECRET_ID,
+  key_secret: process.env.RAZORPAY_SECRET_ID as string,
 });
 
 export async function POST(req: Request) {
   const { amount } = await req.json();
   const order = await razorpay.orders.create({
-    amount: amount * 100,
+    amount: Number(amount) * 100,
     currency: "INR",
+    receipt: `order_${Date.now()}`,
   });
 
   return NextResponse.json(order);
