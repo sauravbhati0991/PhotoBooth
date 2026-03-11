6a26ae36faac3cae85c96bae2c6cae6a9442aa94
Saurav Bhati <sauravbhati0991@gmail.com>
2026-03-12 05:20:29 +0530

    add mongodb4

--- app/api/layouts/[id]/route.ts
+++ app/api/layouts/[id]/route.ts
@@ -1,21 +1,25 @@
 import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
+import { NextRequest } from "next/server";
 
 export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } },
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> },
 ) {
   try {
+    const { id } = await context.params;
+
     const client = await clientPromise;
     const db = client.db("photobooth");
 
     await db.collection("layouts").deleteOne({
-      _id: new ObjectId(params.id),
+      _id: new ObjectId(id),
     });
 
     return Response.json({ success: true });
   } catch (error) {
-    console.error("DELETE layout error:", error);
+    console.error("Delete layout error:", error);
+
     return Response.json({ error: "Failed to delete layout" }, { status: 500 });
   }
 }
